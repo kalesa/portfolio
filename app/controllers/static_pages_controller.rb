fbd9889f9837373ed54ac145423cd1bcae78a9fd
Marissa Abao <iza.abao@gmail.com>
2016-09-18 20:55:37 +0800

    added mailers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def about
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/work'
   get 'static_pages/contact'
   root 'static_pages#about'
+  post 'static_pages/thank_you'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
