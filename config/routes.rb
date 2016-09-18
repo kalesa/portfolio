47d7dc52ae4454dd4faa9eff8348f054e6670139
Marissa Abao <iza.abao@gmail.com>
2016-09-18 15:00:52 +0800

    added static pages and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+  get 'static_pages/work'
+  get 'static_pages/contact'
+  root 'static_pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
